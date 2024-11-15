9cc2d3bfcf8ea575589d5fbbaef9f330de7f48bf
Akashborkar321 <borkara88@gmail.com>
2024-11-16 02:51:43 +0530

    Revert "trying shelve ftr"
    This reverts commit 12ed4d12

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -5,23 +5,4 @@ public class Test {
     public static void test(String maintain){
         System.out.println("Hello mam");
     }
-
-    public void firstFeature(){
-         System.out.println("First feature adding");
-        System.out.println("First feature adding");
-
-        System.out.println("First feature adding");
-
-        System.out.println("First feature adding");
-        System.out.println("First feature adding");
-
-        System.out.println("First feature adding");
-
-    }
-
-
-    public String sayHello(){
-        System.out.println("Good night");
-        return "gn";
-    }
 }
